40ddb80347abae3f15f93f998776440ab2a53a1a
Patrick Bosworth <bosworth.patrick@gmail.com>
2018-02-04 11:48:52 +0000

    latest commit with change for heroku testing

--- server.js
+++ server.js
@@ -3,62 +3,54 @@ const hbs = require('hbs');
 const fs = require('fs');
 
 const port = process.env.PORT || 3000;
-
 var app = express();
 
-hbs.registerPartials(__dirname + '\\views\\partials');
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-
-
 
 app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if (err) {      console.log('Unable to append to server.log')    }
-});
+  fs.appendFile('server.log', log + '\n');
   next();
-})
+});
 
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
-// })
+// });
 
-app.use(express.static(__dirname + "\\public"));
+app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear', ()=> {
-    return new Date().getFullYear()
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt', (text) =>{
+hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
-})
+});
 
 app.get('/', (req, res) => {
-  res.render('home.hbs',{
+  res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'good to see you here!!',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.hbs',{
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
   });
-
 });
 
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send({
-    error: 'not able to connect or something....'
+    errorMessage: 'Unable to handle request'
   });
 });
 
-// route for /bad. simulate a failed request. response.send. JSON data with error property.
-
 app.listen(port, () => {
-  console.log(`server up on ${port}`);
+  console.log(`Server is up on port ${port}`);
 });
